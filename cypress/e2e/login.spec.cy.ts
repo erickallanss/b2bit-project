dd81f494fff9aea322ce696bed6c643c08acc08c
Erick Allan <erickallann@gmail.com>
2024-05-19 21:24:04 -0300

    test: added cypress package and added tests about login and profile pages

---
+++ cypress/e2e/login.spec.cy.ts
@@ -0,0 +1,42 @@
+describe('Testing elements and behavior in login page', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/login');
+  });
+
+  it('Should render login page and find all elements', () => {
+    cy.get('img[alt="B2Bit Logo"]').should('be.visible'); // Verifica se a logo está visível
+    cy.get('form').should('be.visible');
+    cy.get('label').contains('E-mail').should('be.visible');
+    cy.get('input[type="email"]').should('be.visible');
+    cy.get('label').contains('Password').should('be.visible');
+    cy.get('input[type="password"]').should('be.visible');
+    cy.contains('button', 'Sign In').should('be.visible');
+  })
+
+
+  it('should login successfully with valid credentials', () => {
+    cy.intercept(
+      'POST', 
+      '/auth/login', {
+      statusCode: 200,
+    }).as('loginRequest');
+    cy.get('input[type="email"]').type('user@email.com');
+    cy.get('input[type="password"]').type('password');
+    cy.get('button[type="submit"]').click();
+    cy.wait('@loginRequest').its('response.statusCode').should('eq', 200);
+    cy.url().should('include', '/');
+  });
+
+
+  it('should show an error when using invalid credentials to log in', () => {
+    cy.intercept('POST', '/auth/login', {
+      statusCode: 401,
+      body: { message: 'Authentication error. Try again.' }
+    }).as('loginRequest');
+    cy.get('input[type="email"]').type('cliente@youdrive.com');
+    cy.get('input[type="password"]').type('wrongpassword');
+    cy.get('button[type="submit"]').click();
+    cy.wait('@loginRequest').its('response.statusCode').should('eq', 401);
+    cy.get('div').contains('Incorrect email and/or password. Please check your email and password and try again.');
+  });
+});
---
+++ cypress/e2e/profile.spec.cy.ts
@@ -0,0 +1,22 @@
+// cypress/integration/profile.spec.ts
+
+describe('Testing elements and behavior in profile page', () => {
+    beforeEach(() => {
+        cy.visit('http://localhost:3000/login');
+        cy.get('input[type="email"]').type('cliente@youdrive.com');
+        cy.get('input[type="password"]').type('password');
+        cy.get('button[type="submit"]').click();
+    });
+
+    it('should render profile page and show all elements', () => {
+        cy.contains('Profile picture').should('be.visible');
+        cy.get('img[alt="Profile picture"]').should('be.visible');
+        cy.get('label').contains('Your Name').next('input').should('have.value', 'Cliente');
+        cy.get('label').contains('Your E-mail').next('input').should('have.value', 'cliente@youdrive.com');
+    });
+
+    it('should logout successfully', () => {
+        cy.get('button').contains('Logout').click();
+        cy.url().should('include', '/login');
+    });
+});
